ff83cc5ceadef52f61aa88571682d9fa75cff768
Latifah <afralatifah@gmail.com>
2023-01-17 14:41:51 +0700

    grafik terbaru

--- app/Http/Controllers/GrafikController.php
+++ app/Http/Controllers/GrafikController.php
@@ -8,6 +8,39 @@ use Illuminate\Support\Facades\DB;
 
 class GrafikController extends Controller
 {
+    public function tabel(Request $request)
+    {
+        $data['title'] = 'Grafik Ritase';
+        $find = $request->tahun ?? date("01/01/2022 - 12/31/2022");
+
+        $explode = explode("-", $find); //explode untuk memecah data waktu berdasarkan tanda -
+        $start = Carbon::parse($explode[0])->format('Y-m-d') . ' 00:00:01';
+        $data['month_start'] = date('m');
+        $data['start'] = $start;
+
+        $end = Carbon::parse($explode[1])->format('Y-m-d') . ' 23:59:59';
+        $data['month_end'] = date('m');
+        $data['end'] = $end;
+
+
+
+        $tb_transaksi = DB::table("tb_transaksi")
+        // ->select('') berdasarkan field
+        ->join('tb_tujuan', 'tb_tujuan.tujuan_id', '=', 'tb_transaksi.tujuan_id')
+        ->selectRaw("MONTH(check_in) as month, SUM(qty) as qty") //bisa menjalankan function bawaan sql
+        ->whereStatusTransaksi(1)
+        ->whereBetween('check_in', [$start, $end])
+        ->groupByRaw('MONTH(check_in)') // raw bisa menjalankan function
+        ->get();
+
+        $data['transaksi'] = [];
+        foreach ($tb_transaksi as $key => $transaksi) {
+            $data['transaksi'][$transaksi->month] = $transaksi->qty;
+        }
+
+        return view('grafik.index',$data);
+    }
+
     public function index(Request $request)
     {
         $find = $request->tahun ?? date("01/01/2022 - 12/31/2022");
@@ -20,10 +53,11 @@ class GrafikController extends Controller
         $data['end'] = $end;
 
         $tb_transaksi = DB::table("tb_transaksi")
-        ->selectRaw("MONTH(check_in) as month, SUM(qty) as qty")
-        ->whereBetween('check_in', [$start, $end])
+        ->join('tb_tujuan', 'tb_tujuan.tujuan_id', '=', 'tb_transaksi.tujuan_id')
+        ->selectRaw("MONTH(check_in) as month, SUM(qty) as qty") //bisa menjalankan function bawaan sql
         ->whereStatusTransaksi(1)
-        ->groupByRaw('MONTH(check_in)')
+        ->whereBetween('check_in', [$start, $end])
+        ->groupByRaw('MONTH(check_in)') // raw bisa menjalankan function
         ->get();
 
         $data['transaksi'] = [];
@@ -42,35 +76,5 @@ class GrafikController extends Controller
         return ['label' => $label_grafik, 'data' => $data_grafik];
     }
 
-    public function tabel(Request $request)
-    {
-        $data['title'] = 'Home';
-        $find = $request->tahun ?? date("01/01/2022 - 12/31/2022");
 
-        $explode = explode("-", $find); //explode untuk memecah data waktu berdasarkan tanda -
-        $start = Carbon::parse($explode[0])->format('Y-m-d') . ' 00:00:01';
-        $data['month_start'] = date('m');
-        $data['start'] = $start;
-
-        $end = Carbon::parse($explode[1])->format('Y-m-d') . ' 23:59:59';
-        $data['month_end'] = date('m');
-        $data['end'] = $end;
-
-
-
-        $tb_transaksi = DB::table("tb_transaksi")
-        // ->select('') berdasarkan field
-        ->selectRaw("MONTH(check_in) as month, SUM(qty) as qty") //bisa menjalankan function bawaan sql
-        ->whereStatusTransaksi(1)
-        ->whereBetween('check_in', [$start, $end])
-        ->groupByRaw('MONTH(check_in)') // raw bisa menjalankan function
-        ->get();
-
-        $data['transaksi'] = [];
-        foreach ($tb_transaksi as $key => $transaksi) {
-            $data['transaksi'][$transaksi->month] = $transaksi->qty;
-        }
-
-        return view('grafik.index',$data);
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,34 +19,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $data['title'] = 'Login';
-    return view('login',$data);
+    return view('login', $data);
 })->name('login');
 
-Route::post('login','HomeController@auth');
+Route::post('login', 'HomeController@auth');
 
-Route::middleware('admin')->prefix('admin')->group(function ()
-{
+Route::middleware('admin')->prefix('admin')->group(function () {
     Route::get('/', 'HomeController@index')->name('admin');
-    Route::resource('barang','BarangController');
-    Route::resource('satuan','SatuanController');
-    Route::resource('member','MemberController');
-    Route::resource('setting','SettingController');
+    Route::resource('barang', 'BarangController');
+    Route::resource('satuan', 'SatuanController');
+    Route::resource('member', 'MemberController');
+    Route::resource('setting', 'SettingController');
 
 
 
-    Route::resource('tujuan','TujuanController');
+    Route::resource('tujuan', 'TujuanController');
 
-    Route::resource('operator','OperatorController');
+    Route::resource('operator', 'OperatorController');
 
     //checkin
-    Route::resource('adminAb','AdminAbController');
+    Route::resource('adminAb', 'AdminAbController');
     Route::get('/member_select', 'AdminAbController@select_member')->name('member.select');
     Route::get('/kendaraan_select', 'AdminAbController@select_kendaraan')->name('kendaraan.select');
 
-    Route::resource('kendaraan','KendaraanController');
+    Route::resource('kendaraan', 'KendaraanController');
 
-    Route::post('member-point/{id}','MemberController@point')->name('member.point');
-    Route::get('member-status/{id}/{value}','MemberController@status')->name('member.status');
+    Route::post('member-point/{id}', 'MemberController@point')->name('member.point');
+    Route::get('member-status/{id}/{value}', 'MemberController@status')->name('member.status');
     Route::get('transaksi-cek/{id}', 'TransaksiController@cek')->name('transaksi.cek');
     Route::put('transaksi-ceks/{id}', 'TransaksiController@ceks')->name('transaksi.ceks');
     Route::get('transaksi-harian', 'TransaksiController@hari')->name('transaksi.hari');
@@ -64,47 +63,46 @@ Route::middleware('admin')->prefix('admin')->group(function ()
     route::get("cetak-perbarang", [LaporanController::class, 'cetak_perbarang'])->name("cetak.perbarang");
     route::get("excel-perbarang", [LaporanController::class, 'excel_perbarang'])->name("excel.perbarang");
 
+    //grafik
+    Route::get('grafik', [GrafikController::class, 'index'])->name('grafik');
+    Route::get('grafik-ritase', [GrafikController::class, 'tabel'])->name('tabel');
+
     //Point Sopir
-    route::get('point-sopir', [PointController::class,'index'])->name('point.sopir');
+    route::get('point-sopir', [PointController::class, 'index'])->name('point.sopir');
 
 
     Route::get('member_tampil', 'MemberController@member')->name('tampil.member');
     Route::get('delete', 'MemberController@destroy_member')->name('hapus.member');
 });
 
-Route::middleware('operator')->prefix('operator')->group(function ()
-{
+Route::middleware('operator')->prefix('operator')->group(function () {
     Route::get('/', 'HomeController@indexOperator')->name('home.index');
-    Route::resource('transaksi','TransaksiController');
+    Route::resource('transaksi', 'TransaksiController');
     Route::get('transaksi-complete/{id}', 'TransaksiController@complete')->name('transaksi.complete');
     Route::put('transaksi-completes/{id}', 'TransaksiController@completeTransaksi')->name('transaksi.completes');
     Route::get('transaksi-status/{id}/{value}', 'TransaksiController@status')->name('transaksi.status');
 
     //checkout
     Route::get('checkout-op', [CheckoutController::class, 'index_op'])->name('checkout.op');
-    Route::get('checkout/{id}/{value}',[CheckoutController::class, 'status'])->name('checkout.status');
-    Route::get('checkout-ket/{id}',[CheckoutController::class, 'show'])->name('checkout.show');
-    Route::put('checkout-kete/{id}',[CheckoutController::class, 'ket'])->name('checkout.ket');
-
-
-
+    Route::get('checkout/{id}/{value}', [CheckoutController::class, 'status'])->name('checkout.status');
+    Route::get('checkout-ket/{id}', [CheckoutController::class, 'show'])->name('checkout.show');
+    Route::put('checkout-kete/{id}', [CheckoutController::class, 'ket'])->name('checkout.ket');
 });
 
-Route::middleware('operator2')->prefix('operator2')->group(function ()
-{
+Route::middleware('operator2')->prefix('operator2')->group(function () {
     Route::get('/', 'HomeController@indexOperator2')->name('home.index2');
 
     //checkin
-    Route::get('adminAb-op2','AdminAbController@index_op2')->name('adminAb.index.op2');
-    Route::get('create_op2','AdminAbController@create_op2')->name('adminAb.create.op2');
-    Route::post('store_op2','AdminAbController@store_op2')->name('adminAb.store.op2');
+    Route::get('adminAb-op2', 'AdminAbController@index_op2')->name('adminAb.index.op2');
+    Route::get('create_op2', 'AdminAbController@create_op2')->name('adminAb.create.op2');
+    Route::post('store_op2', 'AdminAbController@store_op2')->name('adminAb.store.op2');
     Route::get('/member_select_op2', 'AdminAbController@select_member')->name('member.select.op2');
     Route::get('/kendaraan_select_op2', 'AdminAbController@select_kendaraan')->name('kendaraan.select.op2');
 
 
-    Route::get('transaksi-op2','TransaksiController@index_op2')->name('transaksi.index.op2');
-    Route::get('transaksi-edit-op2','TransaksiController@edit')->name('transaksi.edit.op2');
-    Route::put('transaksi-update-op2','TransaksiController@update')->name('transaksi.update.op2');
+    Route::get('transaksi-op2', 'TransaksiController@index_op2')->name('transaksi.index.op2');
+    Route::get('transaksi-edit-op2', 'TransaksiController@edit')->name('transaksi.edit.op2');
+    Route::put('transaksi-update-op2', 'TransaksiController@update')->name('transaksi.update.op2');
 
 
     Route::get('transaksi-complete_op2/{id}', 'TransaksiController@complete_op2')->name('transaksi.complete.op2');
@@ -112,8 +110,7 @@ Route::middleware('operator2')->prefix('operator2')->group(function ()
     Route::get('transaksi-status/{id}/{value}', 'TransaksiController@status')->name('transaksi.status.op2');
 });
 
-//grafik
-Route::get('grafik', [GrafikController::class, 'index'])->name('grafik');
+
 
 Route::get('/logout', 'HomeController@logout')->name('logout');
 
